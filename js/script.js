66c5fbb545af298a06c0b5888300f12a80839ae2
samuel-santinelli <samuelsantinelli@gmail.com>
2021-12-21 11:37:50 -0300

    Enviando as pastas/arquivos para o repositorio no gitHub

---
+++ js/script.js
@@ -0,0 +1,35 @@
+"use scrict"
+
+// Seção do submenu
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+// Seção da barra de pesquisa
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+// Seção do carrinho de compras
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+// Seção que faz as animações conforme o scroll é acionado para baixo
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
